b5a53bf256fc433b02bacba249071a795da58480
Fabio Cataldo <fabiocataldo@gmail.com>
2020-02-13 11:46:05 +0000

    Starting Project

---
+++ app/src/main/java/com/example/moviesmvvm/single_movie_details/SingleMovie.kt
@@ -0,0 +1,13 @@
+package com.example.moviesmvvm.single_movie_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moviesmvvm.R
+
+class SingleMovie : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+    }
+}
